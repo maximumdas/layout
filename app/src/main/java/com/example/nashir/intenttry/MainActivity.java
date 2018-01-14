2d8669109d84a38b6d6b4ae3e3e31c0cd3c525bb
maximumdas <human.balancar@gmail.com>
2018-01-14 11:38:03 +0700

    coba

---
+++ app/src/main/java/com/example/nashir/intenttry/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.nashir.intenttry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
