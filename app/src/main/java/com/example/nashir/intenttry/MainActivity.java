673344bb66ba49debf7dfd81dc686572399dd56e
maximumdas <human.balancar@gmail.com>
2018-01-14 12:43:03 +0700

    tambahan

--- app/src/main/java/com/example/nashir/intenttry/MainActivity.java
+++ app/src/main/java/com/example/nashir/intenttry/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nashir.intenttry;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.*;
@@ -11,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Intent i =new Intent(MainActivity.this, Terusan.class);
+        i.putExtra("pesan","Mantap gan");
+        Button to =(Button)findViewById(R.id.button);
+        to.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/nashir/intenttry/Terusan.java
+++ app/src/main/java/com/example/nashir/intenttry/Terusan.java
@@ -1,8 +1,32 @@
 package com.example.nashir.intenttry;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.*;
+import android.content.*;
+
 /**
  * Created by Nashir on 14/01/2018.
  */
 
-public class Terusan {
+public class Terusan extends Activity{
+    @Override
+    public void onCreate (Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.layar2);
+        TextView text =(TextView)findViewById(R.id.textView);
+        Button go =(Button)findViewById(R.id.button2);
+        Intent baru=this.getIntent();
+        text.setText("Ini adalah "+baru.getStringExtra("pesan"));
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
 }
